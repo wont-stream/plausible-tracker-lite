3c484bc3153a3eef238aa1429af28cb806e0c58d
S€th <143244075+wont-stream@users.noreply.github.com>
2024-09-26 18:37:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ fetch("https://plausible.local/api/event", {
     body: JSON.stringify({
         domain: "wont.stream", // Domain name of the site in Plausible.
         name: "pageview", // Name of the event.
-        url: window.location.href, // URL of the page where the event was triggered.
+        url: location.href, // URL of the page where the event was triggered.
         referrer: document.referrer, // Referrer for this event.
     })
 })
